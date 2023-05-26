95848d73b344ae717bf8ec5aa7b88465518463de
Harry <harry@flareon.lol>
2023-05-26 13:38:31 +1000

    Optimised login flow, changed var types

--- login/script.js
+++ login/script.js
@@ -1,23 +1,16 @@
 // Major assistance provided by Raycast AI, see README for more info
 function loginFlow() { // Initialises "loginFlow" function
-  const usernameInput = document.getElementById("username").value; // Sets var "usernameInput" to the value of the username field
-  const passwordInput = document.getElementById("password").value; // Sets var "passwordInput" to the value of the password field
-  const loginConfirm = document.getElementById("loginConfirm"); // Sets var "loginConfirm" to the "loginConfirm" paragraph in the HTML
+  let usernameInput = upper(document.getElementById("username").value); // Sets var "usernameInput" to the value of the username field
+  let passwordInput = document.getElementById("password").value; // Sets var "passwordInput" to the value of the password field
+  let loginConfirm = document.getElementById("loginConfirm"); // Sets var "loginConfirm" to the "loginConfirm" paragraph in the HTML
   var now = new Date(); // Sets a variable for the current date
   var expires = new Date(now.getTime() + 24 * 60 * 60 * 1000); // Sets variable for expiration of the login cookie
 
-  if (usernameInput !== "admin") { // Checks if var "usernameInput" does not equal admin
-    loginConfirm.innerHTML = "Username incorrect."; // Changes "loginConfirm" to "Username incorrect." and displays it to the user
-    return;
+  if (usernameInput == "ADMIN") AND (passwordInput == "admin") {
+    document.cookie = "login=true; expires=" + expires.toUTCString(); // Saves (very basic) login cookie to the browser
+    window.location.href = "https://athleticscarnival.flareon.lol/marshallpage"; // Redirects the user to the marshalling page if both fields contain "admin"
   }
 
-  if (passwordInput !== "admin") { // Checks if var "passwordInput" does not equal admin
-    loginConfirm.innerHTML = "Password incorrect."; // Changes "loginConfirm" to "Password incorrect." and displays it to the user
-    return;
-  }
-
-  document.cookie = "login=true; expires=" + expires.toUTCString(); // Saves (very basic) login cookie to the browser
-  window.location.href = "https://athleticscarnival.flareon.lol/marshallpage"; // Redirects the user to the marshalling page if both fields contain "admin"
 }
 // This login method has potentially the worst security ever seen in a web app, second only to Dropbox in 2011 when passwords were optional for a few hours.
 // https://techcrunch.com/2011/06/20/dropbox-security-bug-made-passwords-optional-for-four-hours/
