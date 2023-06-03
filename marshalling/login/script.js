682d8497810ef08cd08ec979b9f6a63c744f5d92
Harry <harry@flareon.lol>
2023-06-03 19:22:30 +1000

    Redirects to marshall if cookie exists on login page

--- marshalling/login/script.js
+++ marshalling/login/script.js
@@ -1,5 +1,11 @@
 // Major assistance provided by Raycast AI, see README for more info
-// URLs need to be changed for Replit
+function checkLogin() {
+  var cookie = document.cookie; // Get the cookie string
+  if (cookie.includes("login=true")) { // Check if the cookie contains "login=true"
+    window.location.href = "/marshalling/marshallpage"; // Redirect to login page if the cookie doesn't exist
+  }
+}
+
 function loginFlow() { // Initialises "loginFlow" function
   let usernameInput = document.getElementById("username").value; // Sets var "usernameInput" to the value of the username field
   let passwordInput = document.getElementById("password").value; // Sets var "passwordInput" to the value of the password field
@@ -19,7 +25,5 @@ function loginFlow() { // Initialises "loginFlow" function
 // This login method does the following wrong:
 // * Stores the user credentials in plain-text as opposed to hashing at least the password
 // * Stores the credentials IN THE CODE, allowing any visitor to inspect element to grab them
-// * Allows the user to simply go to athleticscarnival.flareon.lol/marshallpage and completely bypass the login 💀
-// Ideally, the marshalling page will check that the user has logged in through a cookie of some sort and redirect to the login page if not logged in.
-// I could also hand over the entire login process to Google OAuth and only need to store an email or token on our servers.
-// I don't know how to do any of this, and none of it will be implemented.
+// I could also hand the entire login process to Google OAuth and only need to store an email or token on our servers.
+// I don't know how to do any of this, and none of it will be implemented. 
