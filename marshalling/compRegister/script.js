4b7ab74f293852bcce4bca4a044417dd72784e50
Harry <harrylewandowski04@gmail.com>
2023-07-19 12:22:37 +1000

    Added documentation

--- marshalling/compRegister/script.js
+++ marshalling/compRegister/script.js
@@ -1,7 +1,7 @@
 function registerFlow() {
-    var registerConfirmed = document.getElementById("registerConfirm");
-    registerConfirmed.innerHTML = "Registration successful! You'll be redirected to the home page in 5 seconds.";
-    setTimeout(function () {
+    var registerConfirmed = document.getElementById("registerConfirm"); // Sets var "registerConfirmed" to the "registerConfirm" paragraph in the HTML
+    registerConfirmed.innerHTML = "Registration successful! You'll be redirected to the home page in 5 seconds."; // Displays confirmation message
+    setTimeout(function () { // Redirects to the homepage after 5 seconds
         window.location.href = "/marshalling/marshallpage";
-        }, 5000);
+        }, 5000); // 5000ms -> 5s
     }
--- marshalling/studentReport/script.js
+++ marshalling/studentReport/script.js
@@ -1,11 +1,11 @@
 function studentReportFlow() {
     var selectedOption = document.getElementById("studentSelect").value;
-    var enrolledEvents = "Unknown";
+    var enrolledEvents = "Unknown"; // Sets variables to default "Unknown" value
     var age = "Unknown";
     var gender = "Unknown";
 
-    if (selectedOption == "Ava Sullivan") {
-        enrolledEvents = "100m Sprint, 200m Sprint, Relays";
+    if (selectedOption == "Ava Sullivan") { // Checks for the student being selected
+        enrolledEvents = "100m Sprint, 200m Sprint, Relays"; // Sets variables to the student's data
         age = "16";
         gender = "Female"
     } else if (selectedOption == "Ethan Anderson") {
@@ -13,6 +13,8 @@ function studentReportFlow() {
         age = "17";
         gender = "Male"
     }
+    
+    // Displays the student's data
     var studentData = document.getElementById("studentReport");
     studentData.innerHTML = "Name: " + selectedOption + "<br>Age: " + age + "<br>Enrolled Events: " + enrolledEvents + "<br>Gender: " + gender + "<br>";
 }
