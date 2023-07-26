61cec25facac903a41729a87630efdab39d18546
Harry <harrylewandowski04@gmail.com>
2023-07-26 11:45:28 +1000

    added more data

--- marshalling/studentReport/script.js
+++ marshalling/studentReport/script.js
@@ -1,20 +1,31 @@
 function studentReportFlow() {
     var selectedOption = document.getElementById("studentSelect").value;
-    var enrolledEvents = "Unknown"; // Sets variables to default "Unknown" value
+    var enrolledEvents = "Unknown";
     var age = "Unknown";
     var gender = "Unknown";
 
-    if (selectedOption == "Ava Sullivan") { // Checks for the student being selected
-        enrolledEvents = "100m Sprint, 200m Sprint, Relays"; // Sets variables to the student's data
+    if (selectedOption == "Ava Sullivan") {
+        enrolledEvents = "100m Sprint, 200m Sprint, Relays";
         age = "16";
-        gender = "Female"
+        gender = "Female";
     } else if (selectedOption == "Ethan Anderson") {
         enrolledEvents = "Relays, Shotput";
         age = "17";
-        gender = "Male"
+        gender = "Male";
+    } else if (selectedOption == "James Smith") {
+        enrolledEvents = "100m Sprint, Javelin, High Jump, Long Jump, Triple Jump";
+        age = "15";
+        gender = "Male";
+    } else if (selectedOption == "Olivia Jones") {
+        enrolledEvents = "800m Sprint, 1500m Sprint, Relays";
+        age = "16";
+        gender = "Female";
+    } else if (selectedOption == "William Brown") {
+        enrolledEvents = "400m Sprint, 800m Sprint, 1500m Sprint, Relays";
+        age = "15";
+        gender = "Male";
     }
     
-    // Displays the student's data
     var studentData = document.getElementById("studentReport");
     studentData.innerHTML = "Name: " + selectedOption + "<br>Age: " + age + "<br>Enrolled Events: " + enrolledEvents + "<br>Gender: " + gender + "<br>";
 }
